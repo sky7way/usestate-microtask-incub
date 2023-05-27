5cd409b685900b9575a8a9d0c6907461f9514edb
Aleksandr <sky7way@yandex.ru>
2023-05-27 10:43:56 +0300

    add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let a = 0;
+  const onClickHandler = () => {
+    console.log(++a);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{a}</h1>
+<button onClick={onClickHandler}>incriment</button>
     </div>
   );
 }
