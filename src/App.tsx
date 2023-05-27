27c84972c189e36eeeceaf5088fd91eecd651b94
Aleksandr <sky7way@yandex.ru>
2023-05-27 10:52:48 +0300

    add buttons

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,22 @@ function App() {
     // let a = 0;
 
     let[a, setA]=useState(0);
-    const onClickHandler = () => {
+    const onClickIncrement= () => {
         setA(++a);
+    }
+    const onClickDecrement= () => {
+        setA(--a);
+    }
+    const onClickReset= () => {
+        setA(0);
     }
 
     return (
         <div className="App">
             <h1>{a}</h1>
-            <button onClick={onClickHandler}>incriment</button>
+            <button onClick={onClickIncrement}>increment</button>
+            <button onClick={onClickDecrement}>decrement</button>
+            <button onClick={onClickReset}>reset</button>
         </div>
     );
 }
